1b60a8340e92338ce642e8f70a3707e66015c7f4
Melody Guedel <straying.star@gmx.ch>
2020-12-12 11:22:04 +0100

    added readme

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
-# eventlocation
+CONTENTS OF THIS FILE
+---------------------
+
+ * Introduction
+ 
+Project custom made Content Management System January 2021.
+The project is about an event location where concerts, different kinds of festivals and other parties are held. The location is also available for private parties like weddings.
+
+ * Requirements
+
+- Responsive Layout
+- Database
+- Login Form
+- Register Form
+- Secure Data Transfer
+- Administration Tool, only available trough login
+
+ * Optional
+
+ - Guestbook
+ - News
+ * Installation
+ * Configuration
+ * Troubleshooting
+ * FAQ
+ * Maintainers
---
+++ login.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="Crystal Lake Events. Concerts, Festivals, Weddings and other Events. Book now!">
+
+    <!-- Favicons -->
+
+    
+
+    <!-- Materialize CSS Stylesheet -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    
+    <!-- Other Stylesheets -->
+    <link rel="stylesheet" href="css/main.css">
+    <link rel="stylesheet" href="css/contact.css">
+
+    <!-- Google Fonts -->
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Playfair+Display:wght@800&family=Poppins&display=swap" rel="stylesheet">
+
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css">
+    
+    <!-- Title -->
+    <title>Crystal Lake Events</title>
+
+</head>
+
+<body>
+
+    <!-- Navbar -->
+
+    <?php include "includes/navbar.html"?>
+
+
+
+    <!-- Footer -->
+
+    <?php include "includes/footer.html"?>
+
+    <!-- jQuery -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+
+    <!-- GSAP -->
+
+
+    <!-- Materialize JS -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+
+    <!-- JS Scripts -->
+    <script src="js/code.js"></script>
+
+</body>
+</html>
