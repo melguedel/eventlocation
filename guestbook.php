0b5263af8838f95c8bb40b80a93d8fcc0bf9cd8b
Melody Guedel <straying.star@gmx.ch>
2020-12-12 15:35:00 +0100

    started guestbook with form

--- guestbook.php
+++ guestbook.php
@@ -34,7 +34,37 @@
 
     <?php include "includes/navbar.html"?>
 
+    <!-- Guestbook Login -->
 
+    <!-- Login Form -->
+
+    <form class="logForm" method="POST">
+
+        <h2>Share your thoughts!</h2>
+        <p class="regInfo">You must be logged in to comment</p>
+
+        <label for="userName">Username<input type="text" name="userName" value=""> </label>
+        <div class="redError"></div>
+
+        <label for="password">Password<input type="text" name="password" value=""></label>
+        <div class="redError"></div>
+
+        <div class="row">
+            <form class="col s12">
+            <div class="row">
+                <div class="input-field col s12">
+                <textarea id="textarea1" class="materialize-textarea"></textarea>
+                <label for="textarea1">Textarea</label>
+                </div>
+            </div>
+            </form>
+        </div>
+        <div class="redError"></div>
+
+        <input type="submit" class="subBtn" value="Share your comment"></input>
+
+
+</form>
     
 
     <!-- Footer -->
