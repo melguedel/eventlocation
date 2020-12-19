62ed57669e36fb1a6f712c48906316d1215fde1c
Melody Guedel <straying.star@gmx.ch>
2020-12-19 11:15:05 +0100

    added a bit of formvalidation

--- includes/mysqli-connect.php
+++
@@ -1,9 +0,0 @@
-<?php
-// verbinden mit dem mysql server (Localhost Zugangsdaten: xampp: root {leer} / mamp: root root)
-define("DBSERVER", 'localhost');
-define("DBUSER", 'root');
-define("DBPASSWORT", 'root');
-define("DBNAME", 'admins');
-
-$conn = mysqli_connect(DBSERVER, DBUSER, DBPASSWORT, DBNAME) OR die('DB verbindung hat nicht geklappt: '.mysqli_connect_error());
-?>
--- login.php
+++ login.php
@@ -1,3 +1,9 @@
+<?php
+session_start();
+$_SESSION['status'] = "You are logged in!"
+// include_once('includes/config.inc.php');
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -6,6 +12,24 @@
     <meta name="description" content="Crystal Lake Events. Concerts, Festivals, Weddings and other Events. Book now!">
 
     <!-- Favicons -->
+
+    <link rel="apple-touch-icon" sizes="57x57" href="images/favicons/apple-icon-57x57.png">
+    <link rel="apple-touch-icon" sizes="60x60" href="images/favicons/apple-icon-60x60.png">
+    <link rel="apple-touch-icon" sizes="72x72" href="images/favicons/apple-icon-72x72.png">
+    <link rel="apple-touch-icon" sizes="76x76" href="images/favicons/apple-icon-76x76.png">
+    <link rel="apple-touch-icon" sizes="114x114" href="images/favicons/apple-icon-114x114.png">
+    <link rel="apple-touch-icon" sizes="120x120" href="images/favicons/apple-icon-120x120.png">
+    <link rel="apple-touch-icon" sizes="144x144" href="images/favicons/apple-icon-144x144.png">
+    <link rel="apple-touch-icon" sizes="152x152" href="images/favicons/apple-icon-152x152.png">
+    <link rel="apple-touch-icon" sizes="180x180" href="images/favicons/apple-icon-180x180.png">
+    <link rel="icon" type="image/png" sizes="192x192"  href="images/favicons//android-icon-192x192.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="images/favicons/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="96x96" href="images/favicons/favicon-96x96.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="images/favicons/favicon-16x16.png">
+    <!-- <link rel="manifest" href="/manifest.json"> -->
+    <meta name="msapplication-TileColor" content="#ffffff">
+    <meta name="msapplication-TileImage" content="images/favicons/ms-icon-144x144.png">
+    <meta name="theme-color" content="#ffffff">
 
     
 
@@ -35,13 +59,98 @@
 
     <?php include "includes/navbar.html"?>
 
+
     <!-- Login or Registration -->
+
+    <?php
+
+    require_once('includes/config.inc.php');
+
+    // Variablen erstellen
+    $errorMessage = "";
+    $go = true;
+
+    // Funktion um alle Sonderzeichen, Leerschläge und Tags entfernen
+    function killGerms($str){
+        $str = trim($str);
+        $str = strip_tags($str);
+        $str = htmlspecialchars($str); 
+        return $str;
+    }
+
+    // Funktion um Länge der Eingabe zu validieren
+    function stringLaenge ($str, $feld, $min, $max) {
+        global $errorMessage;
+        global $go;
+
+        // Anzahl der Zeichen checken
+        $laengeZeichen = strlen($str);
+        if ( $laengeZeichen < $min ) {
+            $errorMessage .= "Input in ".$feld." too short! Must at least be ".$min." characters.<br>";
+            $go = false;
+        } elseif ( $laengeZeichen > $max ) {
+            $errorMessage .= "Input in ".$feld." too long! Must be maximum ".$max." characters.<br>";
+            $go = false;
+        } elseif ( empty($_POST['agb']) ) {
+            $errorMessage .= "You did not accept the terms and conditions.<br>";
+            $go = false;
+        }
+    }
+
+
+    // Wurde Submit Button gedrückt?
+    if ( isset($_POST['submit']) ) {
+
+        // "Desinfektion" der Eingaben
+        $usernameValue = killGerms($_POST['username'], FILTER_SANITIZE_STRING);
+        $emailValue = killGerms($_POST['email'], FILTER_SANITIZE_EMAIL);
+        $passwordValue = killGerms($_POST['password'], FILTER_SANITIZE_STRING);
+
+
+        // Länge der Eingabe validieren
+        stringLaenge($usernameValue, "Username", 2, 50);
+        stringLaenge($passwordValue, "Password", 2, 50);
+
+
+        // Passwort verschlüsseln
+        $hashPass = password_hash($passwordValue, PASSWORD_DEFAULT);
+
+
+        // In DB speichern
+        $sql = "INSERT INTO users (first_name, last_name,email, password) VALUE('".$firstName."', '".$surName."', '".$email."','".$hashPassword."')";
+		$result = mysqli_query($conn, $sql);
+        
+
+        // Sind die Eingaben richtig?
+        if ($go && isset($_POST['agb']) && $result ) {
+            // Eingabe richtig!
+            echo "<div class=\"new\">";
+            echo "New registration is done!";
+            echo "</div>\n";
+        } 
+        else {
+            // Angaben falsch
+            echo "<div class=\"redError\">";
+            echo $errorMessage;
+            echo "</div>\n";
+        }
+
+    }
+    
+    else {
+        // Falls noch nichts eingegeben wurde, alles auf leer setzen
+        $usernameValue = "";
+        $emailValue = "";
+        $passwordValue = "";
+    }
+
+    ?>
 
     <section id="loginSection">
 
             <!-- Registration -->
 
-                <form id="registerForm" method="POST" class="logForm">
+                <form action="login.php" id="registerForm" method="POST" class="logForm">
 
                         <h2>Register</h2>
                         <p class="regInfo">and get discounts and goodies for upcoming events!</p>
@@ -73,20 +182,20 @@
                                     <input class="with-gap" name="group3" type="radio" />
                                     <span>Other</span>
                                     </label>
-                                    <div class="redError"></div>
+                                    <!-- <div class="redError"><?=$errorMessage?></div> -->
                                 </p>
 
                             </div>
 
                             <!-- Eingabefelder -->
-                            <label for="username">Username<input type="text" name="username"></label>
-                            <div class="redError"></div>
+                            <label for="username">Username<input type="text" name="username" value="<?=$usernameValue?>"></label>
+                            <!-- <div class="redError"><?=$errorMessage?></div> -->
 
-                            <label for="email">E-Mail<input type="text" name="email"></label>
-                            <div class="redError"></div>
+                            <label for="email">E-Mail<input type="email" name="email" value="<?=$emailValue?>"></label>
+                            <!-- <div class="redError"><?=$errorMessage?></div> -->
 
-                            <label for="passWord">Password<input type="text" name="passWord"></label>
-                            <div class="redError"></div>
+                            <label for="password">Password<input type="text" name="password" value="<?=$passwordValue?>"></label>
+                            <!-- <div class="redError"><?=$errorMessage?></div> -->
 
                             <!-- Terms and Conditions -->
 
@@ -95,15 +204,15 @@
                                     <input type="checkbox" class="filled" name="agb" />
                                     <span>Accept terms and conditions</span>
                                 </label>
-                                <div class="redError"></div>    
+                                <!-- <div class="redError"><?=$errorMessage?></div>     -->
                             </p>
                             
 
                             <!-- Form nicht ausgefüllt -->
-                            <div class="redError"></div>
+                            <!-- <div class="redError"><?=$errorMessage?></div> -->
 
                             <!-- Form korrekt abgeschickt -->
-                            <div class="new"></div>
+                            <!-- <div class="new"></div> -->
 
                             <!-- Submit -->
                             <input type="submit" class="subBtn" name="submit" value="Register"></input>
@@ -113,20 +222,59 @@
 
                     </form>
 
+
+    <?php
+
+    // Wurde Login Button gedrückt?
+    if ( isset($_POST['login']) ) {
+
+        // "Desinfektion" der Eingaben
+        $user = killGerms($_POST['userName'], FILTER_SANITIZE_STRING);
+        $pass = killGerms($_POST['pass'], FILTER_SANITIZE_STRING);
+
+        // Ist Username vorhanden in DB?
+        $sql = "SELECT * FROM users WHERE user = '".$user."'";
+        $rs = mysqli_query($conn, $sql);
+        $numRows = mysqli_num_rows($rs);
+
+        // Ist Passwort korrekt?
+        if ($numRows == 1) {
+            $row = mysqli_fetch_assoc($rs);
+            if (password_verify($pass, $row['pass'])) {
+                // Eingabe richtig!
+                // Umleitung auf Login-Seite
+	            header('Location: dashboard.php');
+            }
+            else {
+            // Falsches Passwort
+            echo "<div class=\"redError\">";
+            echo "Wrong password!";
+            echo "</div>\n";
+            }
+        } 
+        else {
+            // Falscher User
+            echo "<div class=\"redError\">";
+            echo "No user found!";
+            echo "</div>\n";
+        }
+    }
+
+
+    ?>
+
             <!-- Login Form -->
 
-                <form class="logForm" method="POST">
+                <form action="login.php" class="logForm" method="POST">
 
                         <h2>Login</h2>
                         <p class="regInfo">and continue exploring</p>
 
-                        <label for="userName">Username<input type="text" name="userName" value=""> </label>
-                        <div class="redError"></div>
+                        <label for="userName">Username<input type="text" name="userName" value="<?=$user?>"></label>
 
-                        <label for="password">Password<input type="text" name="password" value=""></label>
-                        <div class="redError"></div>
+                        <label for="pass">Password<input type="text" name="pass" value="<?=$pass?>"></label>
                         
-                        <input type="submit" class="subBtn" value="Log me in"></input>
+                        <input type="submit" class="subBtn" name="login" value="Log me in"></input>
 
                         <p class="logMessage">Not registered yet? <a class="toggleBtn">Register here</a></p>
 
